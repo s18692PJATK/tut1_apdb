9471e79a75a51e518b39441addb4d1904c63de9b
s18692 <s18692@pjwstk.edu.pl>
2020-03-06 10:40:53 +0100

    init

---
+++ Tutorial_Solution/Tutorial_Crawler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tutorial_Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Crawler(string url)
+        {
+
+        }
+    }
+}
