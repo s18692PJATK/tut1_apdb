3988d63ce59e48fb8307809e0548de902c9bc8fa
s18692 <s18692@pjwstk.edu.pl>
2020-03-06 11:15:58 +0100

    second commit

--- Tutorial_Solution/Tutorial_Crawler/Program.cs
+++ Tutorial_Solution/Tutorial_Crawler/Program.cs
@@ -1,17 +1,37 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Tutorial_Crawler
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var websiteUrl = args[0];
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z)+[a-z0-9]*@[[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var emails = regex.Matches(htmlContent);
+
+                foreach (var email in emails)
+                {
+                    Console.WriteLine(email.ToString());
+                }
+
+            }
+
+
+
+
         }
 
-        public static void Crawler(string url)
-        {
-
-        }
+        
     }
 }
